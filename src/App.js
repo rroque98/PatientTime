ce5d3e28352046fa82220c115285d89cfd4c87d7
Ricardo <rroque98@gmail.com>
2019-03-15 14:41:59 -0700

    Set up prettier, eslint, babel, parcel, and react

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Renders React Successfully</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
