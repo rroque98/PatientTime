d6a6bccf4defc0a1352a95e1a73759d9bb8b41d2
Ricardo <rroque98@gmail.com>
2019-03-18 16:12:59 -0700

    Add minor adjustments to layout

--- src/components/PhysicianList/PhysicianList.jsx
+++ src/components/PhysicianList/PhysicianList.jsx
@@ -3,9 +3,16 @@ import React from "react";
 const PhysicianList = ({ physicians }) => {
   return (
     <div>
-      {physicians.map(physician => (
-        <div>physician.firstName physician.lastName</div>
-      ))}
+      <h1>Notable</h1>
+      <h3>Physicians</h3>
+      <ul>
+        {physicians.map(physician => (
+          <li key={physician.id}>
+            {physician.lastName}, {physician.firstName}
+          </li>
+        ))}
+      </ul>
+      <button>Logout</button>
     </div>
   );
 };
