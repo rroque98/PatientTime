5591c8447fa8eeff66b2785ee1aa92efa53324ea
Ricardo <rroque98@gmail.com>
2019-03-20 08:23:40 -0700

    Implement server logic with Express

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
+const ctrl = require("../controllers");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("dist/"));
 
 app.get("/physicians", (req, res) => {
-  res.status(200).json({ message: "get physicians ok" });
+  ctrl.getPhysicians((err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).json(data);
+    }
+  });
+});
+
+app.get("/appointments/:physicianId", (req, res) => {
+  ctrl.getAppointments(req.params.physicianId, (err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).json(data);
+    }
+  });
 });
 
 app.listen(port, () => {
