648bc9d4e2b337d6ba5efdec2d3804ce49f7b55f
Ricardo <rroque98@gmail.com>
2019-03-21 12:12:30 -0700

    Add tests to Physician component

---
+++ src/components/Physician/Physician.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from "react-testing-library";
+import Physician from "./index.js";
+
+afterEach(cleanup);
+
+const physician = {
+  id: 1,
+  lastName: "physLastName",
+  firstName: "physFirstName"
+};
+
+test("should apply physician class to li that is currently selected", () => {
+  const { getByText } = render(
+    <Physician physician={physician} currPhysId={1} />
+  );
+  const elem = getByText(`${physician.lastName}, ${physician.firstName}`);
+  expect(elem.parentNode.classList[0]).toBe("physician");
+});
+
+test("should not apply physician class to li that is not selected", () => {
+  const { getByText } = render(
+    <Physician physician={physician} currPhysId={2} />
+  );
+  const elem = getByText(`${physician.lastName}, ${physician.firstName}`);
+  expect(elem.parentNode.classList[0]).toBe(undefined);
+});
+
+test("Physician snapshot", () => {
+  const physComp = render(<Physician physician={{ id: 1, currPhysId: 1 }} />);
+  expect(physComp).toMatchSnapshot();
+});
