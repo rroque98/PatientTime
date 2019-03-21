14368ea652847ee6393aca6dfb8ac4cfe9c1161b
Ricardo <rroque98@gmail.com>
2019-03-21 12:00:11 -0700

    Refactor to add conditional rendering to AppointmentList component and minor css changes

--- src/components/AppointmentList/AppointmentList.jsx
+++ src/components/AppointmentList/AppointmentList.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import "./AppointmentList.css";
 
-const AppointmentList = ({ appointments }) => {
+const AppointmentList = ({ appointments, currPhysFullName, currPhysEmail }) => {
   let count = 0;
   return appointments.length ? (
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Time</th>
-          <th>Kind</th>
-        </tr>
-      </thead>
-      <tbody>
-        {appointments.map(appt => {
-          count++;
-          return (
-            <tr key={appt.name}>
-              <td>{count}</td>
-              <td>{appt.name}</td>
-              <td>{appt.time}</td>
-              <td>{appt.kind}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <React.Fragment>
+      <header className="drHeader">Dr. {currPhysFullName}</header>
+      <div className="email">{currPhysEmail}</div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Time</th>
+            <th>Kind</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map(appt => {
+            count++;
+            return (
+              <tr key={appt.name}>
+                <td>{count}</td>
+                <td>{appt.name}</td>
+                <td>{appt.time}</td>
+                <td>{appt.kind}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </React.Fragment>
   ) : null;
 };
 
