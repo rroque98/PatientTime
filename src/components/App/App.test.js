48a4be3ab932c505f3a4c4f39e71052f14466cbd
Ricardo <rroque98@gmail.com>
2019-03-21 12:06:23 -0700

    Add snapshot test to App component and coverage command

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -7,6 +7,36 @@ import {
 } from "react-testing-library";
 import App from "./index.js";
 
-test("snapshot", () => {
-  //
+afterEach(cleanup);
+const physicians = [
+  {
+    id: 1,
+    firstName: "Morris",
+    lastName: "Coppedge"
+  },
+  {
+    id: 2,
+    firstName: "Evela",
+    lastName: "Lonergan"
+  },
+  {
+    id: 3,
+    firstName: "Random",
+    lastName: "Name"
+  },
+  {
+    id: 4,
+    firstName: "Elizabeth",
+    lastName: "Walker"
+  },
+  {
+    id: 5,
+    firstName: "Rani",
+    lastName: "Peltarik"
+  }
+];
+
+test("App snapshot", () => {
+  const appComp = render(<App />);
+  expect(appComp).toMatchSnapshot();
 });
