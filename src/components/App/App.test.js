1148c5a605a4f5fac66415f327086cbf31250b71
Ricardo <rroque98@gmail.com>
2019-03-20 23:42:48 -0700

    Configure jest for testing

---
+++ src/components/App/App.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from "react-testing-library";
+import App from "./index.js";
+
+test("snapshot", () => {
+  //
+});
