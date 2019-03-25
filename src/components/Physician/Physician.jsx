082c69005824d06310e0fd1cd3540a64bfd125ae
Ricardo <rroque98@gmail.com>
2019-03-24 22:42:57 -0700

    Implement styled components for Physician comp

--- src/components/Physician/Physician.jsx
+++ src/components/Physician/Physician.jsx
@@ -1,21 +1,35 @@
 import React from "react";
-import "./Physician.css";
+import styled from "styled-components";
+
+const AppointmentRow = styled.li`
+  list-style-type: circle;
+  line-height: 2em;
+  color: ${props => props.theme.darkGrey};
+  list-style-position: inside;
+  text-indent: 1em;
+  background-color: ${props => (props.select ? props.theme.notableBlue : null)};
+`;
+
+const ApptText = styled.span`
+  color: ${props => (props.select ? props.theme.white : props.theme.black)};
+`;
 
 const Physician = ({ physician, updateApptsOnClick, currPhysId }) => {
   let classPhys = "";
   if (currPhysId === physician.id) {
-    classPhys += "physician";
+    classPhys += "select";
   }
 
   return (
-    <li
+    <AppointmentRow
+      select={classPhys}
       className={classPhys}
       onClick={e => updateApptsOnClick(e, physician.id)}
     >
-      <span>
+      <ApptText select={classPhys}>
         {physician.lastName}, {physician.firstName}
-      </span>
-    </li>
+      </ApptText>
+    </AppointmentRow>
   );
 };
 
