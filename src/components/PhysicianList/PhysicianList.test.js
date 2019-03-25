99ee663e5c9512730bb1934ea52456dc42f3e046
Ricardo <rroque98@gmail.com>
2019-03-24 22:49:27 -0700

    Update snapshots and tests due to the addition of styled components

--- src/components/PhysicianList/PhysicianList.test.js
+++ src/components/PhysicianList/PhysicianList.test.js
@@ -6,6 +6,7 @@ import {
   waitForElement
 } from "react-testing-library";
 import PhysicianList from "./index.js";
+import "jest-styled-components";
 
 afterEach(cleanup);
 const physicians = [
@@ -43,6 +44,16 @@ test("should find Logout button", () => {
   expect(elem.innerHTML).toBe("Logout");
   fireEvent.click(elem);
 });
+
+// test("Does the Logout button have the correct styles", () => {
+//   const { getByText } = render(
+//     <PhysicianList physicians={physicians} currPhysId={1} />
+//   );
+//   const elem = getByText("Logout");
+//   expect(elem).toHaveSyleRule("display", "block");
+//   expect(elem).toHaveSyleRule("font-size", "1em");
+//   expect(elem).toHaveSyleRule("font-weight", "600");
+// });
 
 test("PhysicianList snapshot", () => {
   const physListComp = render(
