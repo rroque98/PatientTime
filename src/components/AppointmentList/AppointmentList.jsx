368743961dd487e8ebc3c26038e974c254c48b00
Ricardo <rroque98@gmail.com>
2019-03-24 22:46:10 -0700

    Convert css to styled components for AppointmentList

--- src/components/AppointmentList/AppointmentList.jsx
+++ src/components/AppointmentList/AppointmentList.jsx
@@ -1,35 +1,72 @@
 import React from "react";
-import "./AppointmentList.css";
+import styled from "styled-components";
+
+const DrHeader = styled.header`
+  font-size: 2em;
+  font-weight: 800;
+  padding: 1em 0 0 0;
+`;
+
+const Email = styled.div`
+  padding: 0 0 1em 0;
+`;
+
+const AppointmentListWrapper = styled.table`
+  border-collapse: collapse;
+  padding: 0.5em 0 0 0;
+`;
+
+const AppointmentListHeader = styled.thead`
+  background-color: ${props => props.theme.notableBlue};
+  color: ${props => props.theme.white};
+`;
+
+const ColTopic = styled.th`
+  text-align: left;
+  padding: 0.5em;
+`;
+
+const Appointment = styled.tr`
+  :nth-child(even) {
+    background: ${props => props.theme.veryLightGrey};
+  }
+`;
+
+const ApptData = styled.td`
+  border-left: ${props => props.theme.veryLightGrey} 1px solid;
+  border-right: ${props => props.theme.veryLightGrey} 1px solid;
+  padding: 0.5em;
+`;
 
 const AppointmentList = ({ appointments, currPhysFullName, currPhysEmail }) => {
   let count = 0;
   return appointments.length ? (
     <React.Fragment>
-      <header className="drHeader">Dr. {currPhysFullName}</header>
-      <div className="email">{currPhysEmail}</div>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Time</th>
-            <th>Kind</th>
-          </tr>
-        </thead>
+      <DrHeader>Dr. {currPhysFullName}</DrHeader>
+      <Email>{currPhysEmail}</Email>
+      <AppointmentListWrapper>
+        <AppointmentListHeader>
+          <Appointment>
+            <ColTopic>#</ColTopic>
+            <ColTopic>Name</ColTopic>
+            <ColTopic>Time</ColTopic>
+            <ColTopic>Kind</ColTopic>
+          </Appointment>
+        </AppointmentListHeader>
         <tbody>
           {appointments.map(appt => {
             count++;
             return (
-              <tr key={appt.name}>
-                <td>{count}</td>
-                <td>{appt.name}</td>
-                <td>{appt.time}</td>
-                <td>{appt.kind}</td>
-              </tr>
+              <Appointment key={appt.name}>
+                <ApptData>{count}</ApptData>
+                <ApptData>{appt.name}</ApptData>
+                <ApptData>{appt.time}</ApptData>
+                <ApptData>{appt.kind}</ApptData>
+              </Appointment>
             );
           })}
         </tbody>
-      </table>
+      </AppointmentListWrapper>
     </React.Fragment>
   ) : null;
 };
