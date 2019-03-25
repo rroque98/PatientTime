d1ba33a2ff6597c627c1f28aa67029480e9aea81
Ricardo <rroque98@gmail.com>
2019-03-24 22:47:00 -0700

    Convert css to styled components for PhysicianList

--- src/components/PhysicianList/PhysicianList.jsx
+++ src/components/PhysicianList/PhysicianList.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 import Physician from "./../Physician";
-import "./PhysicianList.css";
+import styled from "styled-components";
+
+const TitleHeader = styled.header`
+  color: ${props => props.theme.notableBlue};
+  font-size: 2em;
+  font-weight: 600;
+  padding: 0.5em;
+`;
+
+const PhysListHeader = styled.header`
+  font-size: 1em;
+  font-weight: 600;
+  padding: 1em 1em 0 1em;
+`;
+
+const PhysListWrapper = styled.ul`
+  padding: 0;
+`;
+
+const Button = styled.button`
+  display: block;
+  margin: 0 auto 1em auto;
+  padding: 0.5em 1em;
+  background: ${props => props.theme.notableBlue};
+  color: ${props => props.theme.white};
+  border-radius: 6px;
+  text-align: center;
+  outline: none;
+  font-size: 1em;
+  font-weight: 600;
+  :hover {
+    background: ${props => props.theme.darkBlue};
+  }
+`;
 
 const PhysicianList = ({
   physicians,
@@ -10,9 +43,9 @@ const PhysicianList = ({
 }) => {
   return physicians.length ? (
     <React.Fragment>
-      <header className="notableHeader">notable</header>
-      <div className="physListHeader">PHYSICIANS</div>
-      <ul className="physList">
+      <TitleHeader>PatientTime</TitleHeader>
+      <PhysListHeader>PHYSICIANS</PhysListHeader>
+      <PhysListWrapper>
         {physicians.map(physician => (
           <Physician
             physician={physician}
@@ -21,8 +54,8 @@ const PhysicianList = ({
             key={physician.id}
           />
         ))}
-      </ul>
-      <button onClick={handleLogoutClick}>Logout</button>
+      </PhysListWrapper>
+      <Button onClick={handleLogoutClick}>Logout</Button>
     </React.Fragment>
   ) : null;
 };
